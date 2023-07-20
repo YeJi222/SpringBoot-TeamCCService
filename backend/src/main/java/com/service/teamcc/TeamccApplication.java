ac042fd5dcd3f0eb8e13ced90f537b2e2e860c8b
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-07-20 13:52:18 +0900

    feat: create spring boot(gradle) project

---
+++ backend/src/main/java/com/service/teamcc/TeamccApplication.java
@@ -0,0 +1,13 @@
+package com.service.teamcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamccApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/service/teamcc/TeamccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.teamcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
